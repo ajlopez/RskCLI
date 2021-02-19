315cbeab144f29d6ef10971d49c7afad55d39cc4
ajlopez <webmaster@ajlopez.com>
2021-02-19 10:09:27 -0300

    Improve transfer command

--- test/commands/transfer.js
+++ test/commands/transfer.js
@@ -17,15 +17,23 @@ exports['transfer'] = async function (test) {
             test.deepEqual(options, {});
             
             return '0x010203';
+        },
+        receipt: function (txhash, times) {
+            test.equal(txhash, '0x010203');
+            test.equal(times, 0);
+            
+            return {
+                status: '0x1'
+            }
         }
     };
     
     transfer.useClient(client);
     
-    const tx = await transfer.execute([ 'alice', 'bob', '100000' ]);
+    const txr = await transfer.execute([ 'alice', 'bob', '100000' ]);
     
-    test.ok(tx);
-    test.equal(tx, '0x010203');
+    test.ok(txr);
+    test.deepEqual(txr, { status: '0x1' });
     
     test.done();
 };
@@ -44,15 +52,23 @@ exports['transfer with gas in flag'] = async function (test) {
             test.deepEqual(options, { gas: 100000 });
             
             return '0x010203';
+        },
+        receipt: function (txhash, times) {
+            test.equal(txhash, '0x010203');
+            test.equal(times, 0);
+            
+            return {
+                status: '0x1'
+            }
         }
     };
     
     transfer.useClient(client);
     
-    const tx = await transfer.execute([ 'alice', 'bob', '100000', '-g', 100000 ]);
+    const txr = await transfer.execute([ 'alice', 'bob', '100000', '-g', 100000 ]);
     
-    test.ok(tx);
-    test.equal(tx, '0x010203');
+    test.ok(txr);
+    test.deepEqual(txr, { status: '0x1' });
     
     test.done();
 };
