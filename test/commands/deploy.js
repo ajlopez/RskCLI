4b1de1d2819e443d3dbfbb2fa4364b8b6083ea58
ajlopez <webmaster@ajlopez.com>
2021-02-21 11:16:11 -0300

    Deploy command with constructor types and arguments

--- test/commands/deploy.js
+++ test/commands/deploy.js
@@ -3,6 +3,7 @@ const deploy = require('../../lib/commands/deploy');
 const configs = require('../../lib/config');
 const utils = require('../../lib/utils');
 const newaccount = require('../../lib/commands/newaccount');
+const simpleabi = require('simpleabi');
 
 const path = require('path');
 
@@ -17,9 +18,9 @@ exports['deploy'] = async function (test) {
     const client = {
         deploy: function (from, bytecode, args, options) {
             test.deepEqual(from, config.accounts.alice);
-            test.equal(bytecode, contract.bytecode);
-            test.deepEqual(args, []);
             test.deepEqual(options, {});
+            test.deepEqual(args, []);
+            test.equal(bytecode, contract.bytecode);
             
             return '0x010203';
         },
@@ -51,6 +52,50 @@ exports['deploy'] = async function (test) {
     test.done();
 };
 
+exports['deploy with constructor types and arguments'] = async function (test) {
+    newaccount.execute([ 'alice' ]);   
+    
+    const config = configs.loadConfiguration();
+    
+    const cpath = path.join(__dirname, '..', 'contracts');
+    const contract = require(path.join(cpath, 'build', 'contracts', 'Message.json'));
+    
+    const client = {
+        deploy: function (from, bytecode, args, options) {
+            test.deepEqual(from, config.accounts.alice);
+            test.deepEqual(args, [ 'Hello' ]);
+            test.deepEqual(options, { types: [ 'string' ]});
+            test.equal(bytecode, contract.bytecode);
+            
+            return '0x010203';
+        },
+        receipt: function (txhash, times) {
+            test.equal(txhash, '0x010203');
+            test.equal(times, 0);
+            
+            return {
+                contractAddress: '0x040506',
+                status: '0x1'
+            }
+        }
+    };
+    
+    deploy.useClient(client);
+    
+    const caddress = await deploy.execute([ 'alice', 'message', 'Message', 'string', 'Hello', cpath ]);
+    
+    test.ok(caddress);
+    test.equal(caddress, '0x040506');
+    
+    const newconfig = configs.loadConfiguration();
+    
+    test.deepEqual(newconfig.instances.counter, {
+        address: '0x040506',
+        contract: 'Counter'
+    });
+    
+    test.done();
+};
 exports['deploy with gas in options'] = async function (test) {
     newaccount.execute([ 'alice' ]);   
     
