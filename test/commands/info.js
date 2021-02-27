4c6a6de9ce0cb4173d319da568ca74810a3afa83
ajlopez <webmaster@ajlopez.com>
2021-02-27 19:10:33 -0300

    Info command using another provider and log flag

--- test/commands/info.js
+++ test/commands/info.js
@@ -31,6 +31,31 @@ exports['get host, block number, chain id, network version and client version'] 
     test.done();
 };
 
+exports['using another host/provider'] = async function (test) {
+    sethost.execute([ 'http://localhost:4444' ]);
+    const config = configs.loadConfiguration();
+    
+    const provider = createProvider();
+    
+    provider.eth_blockNumber = function () { return 42; };
+    provider.eth_chainId = function () { return 33; };
+    provider.net_version = function () { return 144; };
+    provider.web3_clientVersion = function () { return 'version'; };
+        
+    const result = await info.execute([ provider ]);
+    
+    test.ok(result);
+    test.deepEqual(result, { 
+        host: 'http://localhost:4444',
+        blockNumber: 42,
+        chainId: 33,
+        networkVersion: 144,
+        clientVersion: 'version'
+    });
+    
+    test.done();
+};
+
 function createProvider() {
 	return {
 		call: function (method, args, cb) {
