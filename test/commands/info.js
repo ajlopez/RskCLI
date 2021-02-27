5336199be68f66d88246cff06a75cf5d313272a1
ajlopez <webmaster@ajlopez.com>
2021-02-27 19:01:08 -0300

    Info command retruns network version and client version

--- test/commands/info.js
+++ test/commands/info.js
@@ -4,7 +4,7 @@ const configs = require('../../lib/config');
 const sethost = require('../../lib/commands/sethost');
 const rskapi = require('rskapi');
 
-exports['get host, block number and chain id'] = async function (test) {
+exports['get host, block number, chain id, network version and client version'] = async function (test) {
     sethost.execute([ 'http://localhost:4444' ]);
     const config = configs.loadConfiguration();
     
@@ -12,6 +12,8 @@ exports['get host, block number and chain id'] = async function (test) {
     
     provider.eth_blockNumber = function () { return 42; };
     provider.eth_chainId = function () { return 33; };
+    provider.net_version = function () { return 144; };
+    provider.web3_clientVersion = function () { return 'version'; };
         
     info.useClient(rskapi.client(provider));
     
@@ -21,7 +23,9 @@ exports['get host, block number and chain id'] = async function (test) {
     test.deepEqual(result, { 
         host: 'http://localhost:4444',
         blockNumber: 42,
-        chainId: 33
+        chainId: 33,
+        networkVersion: 144,
+        clientVersion: 'version'
     });
     
     test.done();
