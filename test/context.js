6074899366acf4f234b4f50eb0b27b2b168b9ba3
ajlopez <webmaster@ajlopez.com>
2021-02-28 10:29:34 -0300

    Context retrieves commadns and local require

--- test/context.js
+++ test/context.js
@@ -4,6 +4,8 @@ const rskapi = require('rskapi');
 const utils = require('../lib/utils');
 const configs = require('../lib/config');
 
+const block = require('../lib/commands/block');
+
 exports['context exposes rskapi'] = function (test) {
     test.strictEqual(context.rskapi, rskapi);
 };
@@ -15,3 +17,16 @@ exports['context exposes utils'] = function (test) {
 exports['context exposes configs'] = function (test) {
     test.strictEqual(context.configs, configs);
 };
+
+exports['context retrieves command'] = function (test) {
+    test.strictEqual(context.command('block'), block);
+};
+
+exports['context require internal local module'] = function (test) {
+    test.strictEqual(context.require('./config'), configs);
+};
+
+exports['context require dependency module'] = function (test) {
+    test.strictEqual(context.require('simpleabi'), require('simpleabi'));
+};
+
