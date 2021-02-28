8a06a3ecd754f43219c6a98a5fff2b66991aa703
ajlopez <webmaster@ajlopez.com>
2021-02-28 10:20:49 -0300

    Context exposes config module as configs

--- test/context.js
+++ test/context.js
@@ -2,6 +2,7 @@
 const context = require('../lib/context');
 const rskapi = require('rskapi');
 const utils = require('../lib/utils');
+const configs = require('../lib/config');
 
 exports['context exposes rskapi'] = function (test) {
     test.strictEqual(context.rskapi, rskapi);
@@ -11,3 +12,6 @@ exports['context exposes utils'] = function (test) {
     test.strictEqual(context.utils, utils);
 };
 
+exports['context exposes configs'] = function (test) {
+    test.strictEqual(context.configs, configs);
+};
