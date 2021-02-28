fd25eacbf363e0a1541578a572fb4ed869ae628c
ajlopez <webmaster@ajlopez.com>
2021-02-28 14:38:14 -0300

    Setaddress command using explicit private key

--- test/commands/setaccount.js
+++ test/commands/setaccount.js
@@ -15,6 +15,12 @@ const accs = [
   '0x54650c91f6cd0cf3fc784c87cb26180c26bfb1e8'
 ];
 
+const account = {
+    "privateKey": "0x6f381a30325494a9f89804911616efeedf638bc9b777dd32098e32a8d33d3b5a",
+    "publicKey": "0xd3bec3a05fc449801ee8612d41a5afa65dddf793260021008e93e318740b483bb0a153bd877b8283bfb15616cb506d4e9dd9c93ed465bbf76727fb7f2c2414b3",
+    "address": "0xb090408afb46eb0b27334deb8d674b4cc7de2d42"
+};
+
 exports['set account'] = async function (test) {
     const client = {
         accounts: function () { return accs; }
@@ -34,12 +40,6 @@ exports['set account'] = async function (test) {
 };
 
 exports['set account using address'] = async function (test) {
-    const client = {
-        accounts: function () { return accs; }
-    };
-    
-    setaccount.useClient(client);
-    
     const result = await setaccount.execute([ 'alice', accs[1] ]);
     
     test.equal(result, accs[1]);
@@ -51,3 +51,14 @@ exports['set account using address'] = async function (test) {
     test.done();
 };
 
+exports['set account using private key'] = async function (test) {
+    const result = await setaccount.execute([ 'alice', account.privateKey ]);
+    
+    test.deepEqual(result, account);
+    
+    const config = configs.loadConfiguration();
+
+    test.deepEqual(config.accounts.alice, account);
+    
+    test.done();
+};
