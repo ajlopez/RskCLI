6a6c3eaaf4b64680f7e87adc6308dcf44d404a4e
ajlopez <webmaster@ajlopez.com>
2021-02-28 10:19:15 -0300

    Context exposes utils module

--- test/context.js
+++ test/context.js
@@ -1,7 +1,13 @@
 
 const context = require('../lib/context');
 const rskapi = require('rskapi');
+const utils = require('../lib/utils');
 
 exports['context exposes rskapi'] = function (test) {
     test.strictEqual(context.rskapi, rskapi);
 };
+
+exports['context exposes utils'] = function (test) {
+    test.strictEqual(context.utils, utils);
+};
+
