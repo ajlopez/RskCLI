e4539fd274e4ceb52de83fc81128f12d8387baf7
ajlopez <webmaster@ajlopez.com>
2021-03-05 16:16:57 -0300

    Code command using alias

--- test/commands/code.js
+++ test/commands/code.js
@@ -1,5 +1,6 @@
 
 const code = require('../../lib/commands/code');
+const newaccount = require('../../lib/commands/newaccount');
 const configs = require('../../lib/config');
 
 exports['get code'] = async function (test) {
@@ -23,3 +24,25 @@ exports['get code'] = async function (test) {
     test.done();
 };
 
+exports['get code using alias'] = async function (test) {
+    newaccount.execute([ 'contract' ]);   
+    
+    const config = configs.loadConfiguration();
+    
+    const client = {
+        code: function (hash) {
+            test.equal(hash, config.accounts.contract.address);
+            
+            return '0x1000';
+        }
+    };
+    
+    code.useClient(client);
+    
+    const result = await code.execute([ 'contract' ]);
+    
+    test.ok(result);
+    test.equal(result, '0x1000');
+    
+    test.done();
+};
