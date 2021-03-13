c1efd050ef77e3eda2e5fbd3f359f00b0b81823d
ajlopez <webmaster@ajlopez.com>
2021-03-13 11:24:00 -0300

    info command calculates blocks per hour

--- test/commands/info.js
+++ test/commands/info.js
@@ -4,6 +4,17 @@ const configs = require('../../lib/config');
 const sethost = require('../../lib/commands/sethost');
 const rskapi = require('rskapi');
 
+// from https://ethereum.stackexchange.com/questions/11444/web3-js-with-promisified-api
+
+const promisify = (inner) =>
+  new Promise((resolve, reject) =>
+    inner((err, res) => {
+      if (err) { reject(err) }
+
+      resolve(res);
+    })
+);
+
 exports['get host, block number, chain id, network version and client version'] = async function (test) {
     sethost.execute([ 'http://localhost:4444' ]);
     const config = configs.loadConfiguration();
@@ -79,6 +90,37 @@ exports['get host, block number, chain id, network version and client version us
         networkVersion: 144,
         clientVersion: 'version'
     });
+    
+    test.done();
+};
+
+exports['get block velocity after save'] = async function (test) {
+    sethost.execute([ 'http://localhost:4444' ]);
+    const config = configs.loadConfiguration();
+    
+    delete config.info;
+    
+    configs.saveConfiguration(config);
+    
+    const provider = createProvider();
+    
+    provider.eth_blockNumber = function () { return 42; };
+    provider.eth_chainId = function () { return 33; };
+    provider.net_version = function () { return 144; };
+    provider.web3_clientVersion = function () { return 'version'; };
+        
+    info.useClient(rskapi.client(provider));
+    
+    await info.execute([ '--save' ]);
+
+    provider.eth_blockNumber = function () { return 100; };
+    
+    await promisify(cb => setTimeout(function () { cb(null, null); }, 1000));
+
+    const result = await info.execute([]);
+
+    test.ok(result);
+    test.ok(result.blocksPerHour);
     
     test.done();
 };
