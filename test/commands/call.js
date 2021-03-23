2812eb1c92bcb2e8fd510cd3f73e3fa7cd186224
ajlopez <webmaster@ajlopez.com>
2021-03-23 13:54:45 -0300

    Using rskapi@0.0.18; call command processes bytes32 as output

--- test/commands/call.js
+++ test/commands/call.js
@@ -69,6 +69,37 @@ exports['call returning string'] = async function (test) {
     test.done();
 };
 
+exports['call returning bytes32'] = async function (test) {
+    newaccount.execute([ 'alice' ]);   
+    newaccount.execute([ 'contract' ]);   
+    
+    const config = configs.loadConfiguration();
+    
+    const cpath = path.join(__dirname, '..', 'contracts');
+    const contract = require(path.join(cpath, 'build', 'contracts', 'Counter.json'));
+    
+    const client = {
+        call: function (from, to, fn, args, options) {
+            test.deepEqual(from, config.accounts.alice);
+            test.equal(to, config.accounts.contract.address);
+            test.deepEqual(options, {});
+            test.equal(fn, "bar(string,uint256)");
+            test.deepEqual(args, [ 'foo', 42 ]);
+            
+            return '0x123456789abcdef0123456789abcdef0123456789abcdef0' ;
+        }
+    };
+    
+    call.useClient(client);
+    
+    const result = await call.execute([ 'alice', 'contract', 'bar(string,uint256)', "foo,42", 'bytes32' ]);
+    
+    test.ok(result);
+    test.deepEqual(result, '0x123456789abcdef0123456789abcdef0123456789abcdef0');
+    
+    test.done();
+};
+
 exports['call returning two integers'] = async function (test) {
     newaccount.execute([ 'alice' ]);   
     newaccount.execute([ 'contract' ]);   
