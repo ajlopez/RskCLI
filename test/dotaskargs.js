64d38033cb9d0e305f70a0e2764c1a491237c297
ajlopez <webmaster@ajlopez.com>
2021-03-24 10:51:38 -0300

    execute command removes script name from arguments

--- test/dotaskargs.js
+++ test/dotaskargs.js
@@ -1,9 +1,8 @@
 
 module.exports = async function (context, args) {
     if (context.configs === require('../lib/config') &&
-        args[0].endsWith('dotaskargs') &&
-        args[1] === 'foo' &&
-        args[2] === 'bar')
+        args[0] === 'foo' &&
+        args[1] === 'bar')
         return true;
     else
         return false;
